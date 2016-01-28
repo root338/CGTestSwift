a0c7e78f05afe3656c404f395e0e24dbae2ce7b6
dj <410500921@qq.com>
2016-01-28 18:36:48 +0800

    create CGTestSwift project

---
+++ TestSwift/Mudule/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestSwift
+//
+//  Created by DY on 16/1/28.
+//  Copyright © 2016年 -. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+}
+
